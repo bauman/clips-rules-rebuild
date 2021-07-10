aa17b2cbe151cf4ce352ba967d08ff63f5ca1578
Dan Bauman <dan@bauman.space>
2021-07-09 19:49:44 -0500

    631 (#1)
    631

---
+++ clipspy-0.3.3/clips/clips_build.py
@@ -0,0 +1,20 @@
+from cffi import FFI
+
+ffibuilder = FFI()
+
+with open("lib/clips.c") as source_file:
+    CLIPS_SOURCE = source_file.read()
+
+with open("lib/clips.cdef") as cdef_file:
+    CLIPS_CDEF = cdef_file.read()
+
+ffibuilder.set_source("_clips",
+                      CLIPS_SOURCE,
+                      libraries=["clips"],
+                      include_dirs=["../core"])
+
+ffibuilder.cdef(CLIPS_CDEF)
+
+
+if __name__ == "__main__":
+    ffibuilder.compile(verbose=True)
