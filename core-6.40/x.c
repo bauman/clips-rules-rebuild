cd07310f4f42b5df312e20c7b982ce926d170c17
Dan Bauman <dan@bauman.space>
2021-07-25 12:42:57 -0500

    upgrade

--- core-6.40/x.c
+++
@@ -1,9 +0,0 @@
-/*
-
-this file is included for the sole purpose of being a shim in the CMakeLists conversion
-
-the add_lbrary functions require at least one source file and the real source files are bundled as archives.
-
-This file should never have any new code in here, but are listed as the primary source for the clips shard library and clips-static library
-
-*/
